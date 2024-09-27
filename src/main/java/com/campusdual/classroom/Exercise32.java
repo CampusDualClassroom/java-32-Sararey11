77b56eb69690e2e2cc17ea663a0a7ecf37e9fbb1
Sara <sarareyvalino23@gmail.com>
2024-09-27 10:41:00 +0200

    Ejercicio completado

--- src/main/java/com/campusdual/classroom/Exercise32.java
+++ src/main/java/com/campusdual/classroom/Exercise32.java
@@ -3,17 +3,21 @@ package com.campusdual.classroom;
 import com.campusdual.util.Utils;
 
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Exercise32 {
 
     public static void main(String[] args) {
-
+        printToFile("Hola");
+        printToFile("Me gustan los videojuegos");
     }
 
     public static String generateStringToSave(String string) {
-
+        String result = "Inicio-" + string + "-Fin";
+        result = string;
+        return string;
     }
 
     private static String generateUserInputToSave(){
@@ -27,7 +31,19 @@ public class Exercise32 {
     }
 
     public static void printToFile(String string) {
+        try {
 
+            FileWriter writer = new FileWriter("src/main/resources/data.txt");
+
+            writer.write(string);
+
+            writer.close();
+
+            System.out.println("El string ha sido escrito en el archivo");
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al escribir en el archivo.");
+            e.printStackTrace();
+        }
     }
 
 
